1f36b646d9afb2a4c8a0d269c983ca384ca69731
Matthew McIntyre <mattymac2@gmail.com>
2021-04-26 17:11:52 +1200

    integrate api call to barcode

--- src/App.js
+++ src/App.js
@@ -3,26 +3,42 @@ import Barcode from "react-barcode";
 
 import "./App.css";
 
-const hardcodedNumber = 1;
-
 function App() {
-  const [timer, setTimer] = useState(30);
+  const [timer, setTimer] = useState(0);
+  const [barcodeValue, setBarcodeValue] = useState(null);
 
   useEffect(() => {
-    if (timer < 1) return;
+    if (!barcodeValue) return fetchBarcodeValue();
+    if (!timer) return;
 
     const interval = setInterval(() => {
       setTimer(timer - 1);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [timer]);
+  }, [timer, barcodeValue]);
+
+  const fetchBarcodeValue = () => {
+
+    setBarcodeValue(null);
+
+    fetch('https://jet-gull-7204.twil.io/generate-barcode').then((res) => {
+      return res.json();
+    }).then((data) => {
+      setBarcodeValue(JSON.stringify(data.barcode));
+      setTimer(30);
+    }).catch((err) => {
+      console.log(err);
+      setBarcodeValue('error');
+    })
+  };
 
   return (
     <div className="App">
       <h1>Barcode</h1>
-      <Barcode value={hardcodedNumber} />
+      {barcodeValue ? <Barcode value={barcodeValue} /> : <span>Fetching Barcode</span>}
       <h1>00:{timer < 10 ? `0${timer}` : timer}</h1>
+      {!timer && <button onClick={() => fetchBarcodeValue()}>Fetch Barcode</button>}
     </div>
   );
 }
