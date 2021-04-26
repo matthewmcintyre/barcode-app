9981dfc87e4c649e7a86f9f47721845d1fb8eef1
Matthew McIntyre <mattymac2@gmail.com>
2021-04-26 16:02:00 +1200

    add react-barcode and render on app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Barcode from "react-barcode";
+
+import "./App.css";
+
+const hardcodedNumber = 1;
 
 function App() {
+  const [timer, setTimer] = useState(30);
+
+  useEffect(() => {
+    if (timer < 1) return;
+
+    const interval = setInterval(() => {
+      setTimer(timer - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timer]);
+
   return (
     <div className="App">
       <h1>Barcode</h1>
+      <Barcode value={hardcodedNumber} />
+      <h1>00:{timer < 10 ? `0${timer}` : timer}</h1>
     </div>
   );
 }
